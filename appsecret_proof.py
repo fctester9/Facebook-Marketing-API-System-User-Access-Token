dbced4505d59e70f2140247388a4ff6bde152838
mofa <mfarroukh0@gmail.com>
2019-09-21 19:38:50 +0400

    Update appsecret_proof.py

--- appsecret_proof.py
+++ appsecret_proof.py
@@ -1,5 +1,5 @@
 import hmac
 import hashlib
 
-h = hmac.new(b'Enter your accesss token and leave the b on the left it is not a typo)', 'Enter your app secret'.encode('utf-8'), hashlib.sha256 )
+h = hmac.new(b'Enter your accesss token and leave the b on the left it is not a typo', 'Enter your app secret'.encode('utf-8'), hashlib.sha256 )
 print( h.hexdigest() )
